2bf25d5ee654d280cd92547a7661a414369b5573
Olivia Cubela <oliviacubela@gmail.com>
2020-06-19 15:44:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,5 @@
 class Dog
-  def initialize(breed)
-    @breed = breed
-  end
 
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
 end
 
-lassie = Dog.new
-lassie.breed = "Collie"
+
